1bedc3b330c236183d833a930e43d90d832faf91
JackJao <766056142@qq.com>
2018-06-26 16:35:04 +0800

    增加tomcat部署支持

--- src/main/java/com/jaon/demo/AppTomcat.java
+++ src/main/java/com/jaon/demo/AppTomcat.java
@@ -1,25 +1,24 @@
-//package com.jaon.demo;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.builder.SpringApplicationBuilder;
-//import org.springframework.boot.web.servlet.ServletComponentScan;
-//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-//
-///**
-// * @author Administrator
-// * 打包成war包的时候需要注释App
-// */
-//@SpringBootApplication
-//@ServletComponentScan(basePackages={"com.jaon.servlet"})
-//public class AppTomcat extends SpringBootServletInitializer {
-//
-//	@Override
-//	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-//		return super.configure(builder);
-//	}
-//
-//	public static void main(String[] args) {
-//		SpringApplication.run(AppTomcat.class, args);
-//	}
-//}
+package com.jaon.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * @author Administrator
+ * 打包成war包的时候需要注释App
+ */
+@SpringBootApplication
+public class AppTomcat extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return super.configure(builder);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppTomcat.class, args);
+	}
+}
