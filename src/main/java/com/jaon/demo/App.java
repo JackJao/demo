96d231db0ea52f1cbf05ab9e5db07847de0fbecf
JackJao <766056142@qq.com>
2018-06-26 15:42:41 +0800

    增加tomcat部署支持

--- src/main/java/com/jaon/demo/App.java
+++ src/main/java/com/jaon/demo/App.java
@@ -2,6 +2,7 @@ package com.jaon.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 /**
  * @author Administrator
