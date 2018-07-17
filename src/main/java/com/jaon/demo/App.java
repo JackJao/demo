85224045e055f29997e055f288dfac39426f21a0
JackJao <766056142@qq.com>
2018-07-17 16:48:44 +0800

    取消注释

--- src/main/java/com/jaon/demo/App.java
+++ src/main/java/com/jaon/demo/App.java
@@ -1,16 +1,16 @@
-//package com.jaon.demo;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.web.servlet.ServletComponentScan;
-//
-///**
-// * @author Administrator
-// */
-//@SpringBootApplication
-//public class App {
-//
-//	public static void main(String[] args) {
-//		SpringApplication.run(App.class, args);
-//	}
-//}
+package com.jaon.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+/**
+ * @author Administrator
+ */
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
