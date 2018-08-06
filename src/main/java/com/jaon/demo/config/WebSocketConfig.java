0d2745e94f0a3f36ee19681fe1fd21b79b1b72e8
JackJao <766056142@qq.com>
2018-08-06 17:17:45 +0800

    SockJS支持添加，添加websocket_sockjs.html 增加一个处理器，在原处理器上追加.withSockJS()

--- src/main/java/com/jaon/demo/config/WebSocketConfig.java
+++ src/main/java/com/jaon/demo/config/WebSocketConfig.java
@@ -19,8 +19,9 @@ public class WebSocketConfig implements WebSocketConfigurer{
     public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
         //添加socket注册处理器
         //配置websocket访问地址：String url = "ws://localhost:8080/websocket/marco";
-       // webSocketHandlerRegistry.addHandler(marcoHandler(),"/websocket/marco");
-        webSocketHandlerRegistry.addHandler(marcoHandler(),"/websocket/marco");//.withSockJS();//如果WebSocket不可用的话， SockJS的备用方案就会发挥作用
+        webSocketHandlerRegistry.addHandler(marcoHandler(),"/websocket/marco");
+        //如果WebSocket不可用的话， SockJS的备用方案就会发挥作用
+        webSocketHandlerRegistry.addHandler(marcoHandler(),"/websocket/marco").withSockJS();
     }
 
 
