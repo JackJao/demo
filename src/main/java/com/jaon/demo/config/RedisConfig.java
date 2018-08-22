bbd92619c8de6416725cc883a85ffa32a4bd585c
JackJao <766056142@qq.com>
2018-08-22 11:09:53 +0800

    更新redis配置和logback.xml配置

--- src/main/java/com/jaon/demo/config/RedisConfig.java
+++ src/main/java/com/jaon/demo/config/RedisConfig.java
@@ -5,6 +5,8 @@ import com.jaon.demo.domain.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -20,9 +22,14 @@ public class RedisConfig {
     @Bean
     public RedisConnectionFactory redisConnectionFactory(){
         //如果什么参数都不设置，默认连接本地6379端口
-        JedisConnectionFactory factory = new JedisConnectionFactory();
-        factory.setPort(6379);
-        factory.setHostName("192.168.0.80");
+        RedisStandaloneConfiguration cfg = new RedisStandaloneConfiguration("192.168.0.80",6379);
+        //选择数据库
+        cfg.setDatabase(5);
+        //默认密码为空
+        cfg.setPassword(RedisPassword.none());
+        //cfg.setPassword(RedisPassword.of("密码"));
+        //加载配置
+        JedisConnectionFactory factory = new JedisConnectionFactory(cfg);
         return factory;
     }
 
