31bf4c55e99a5010ea08b5a48274c154a71212f3
JackGao <766056142@qq.com>
2020-09-02 11:38:00 +0800

    完善druid配置

--- src/main/java/com/jaon/demo/config/SecurityConfig.java
+++ src/main/java/com/jaon/demo/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -18,10 +19,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        //忽略druid管理的url访问，权限释放
+        //让security的csrf校验忽略druid请求。
+        http.csrf().ignoringAntMatchers("/druid/*");
         //通过spring security 的表单登录
-        http.authorizeRequests().anyRequest().authenticated().and().formLogin();
+         http.authorizeRequests().anyRequest().authenticated().and().formLogin()
+                 //开启自动配置的登录功能，如果没有登录或者没有权限就会来到登录页面,会访问/login
+                 .usernameParameter("admin").passwordParameter("123456");
         //通过spring security 的对话框登录
-//        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+        //http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+        //开启自动配置的注销功能，会访问/logout请求
+        //注销成功后返回到首页/
+        http.logout().logoutSuccessUrl("/");
+        //开启记住我功能
+        http.rememberMe();
     }
 
     /**
